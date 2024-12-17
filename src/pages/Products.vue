f380946c2bb2a19d40301a34df58e6b4193704fc
Егор Ш <egorshatalov2002@mail.ru>
2024-12-17 22:59:20 +0300

    Add pagination

--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,18 +1,21 @@
 <script>
 import axios from 'axios';
 import Card from '../components/Card.vue';
-import Aside from '../components/Aside.vue';
+import Sidebar from '../components/Sidebar.vue';
 
 export default {
 name: "products",
 components: {
     Card,  // Добавляем компонент в раздел components
-    Aside,
+    Sidebar,
+
   },
 
   data() {
     return {
       products: [], // Это будет массив с продуктами
+      pagination: [],
+
     };
   },
 
@@ -25,14 +28,31 @@ mounted() {
 
 methods: {
 
+  goToPage(page) {
+    if (page >= 1 && page <= this.pagination.last_page) {
+      this.getProducts(page);
+    }
+  },
+  nextPage() {
+    if (this.pagination.current_page < this.pagination.last_page) {
+      this.getProducts(this.pagination.current_page + 1);
+    }
+  },
+  prevPage() {
+    if (this.pagination.current_page > 1) {
+      this.getProducts(this.pagination.current_page - 1);
+    }
+  },
+
   applyFilters(filterData) {
     this.products = filterData;
   },
-    getProducts() {
-        axios.get('http://localhost:8876/api/products')
+    getProducts(page=1) {
+        axios.post('http://localhost:8876/api/products/filter', {page})
         .then( res => {
             this.products = res.data.data;  // Сохраняем полученные продукты
-            console.log(res.data.data)
+            console.log(res.data)
+            this.pagination = res.data.meta;
             this.getByName();
         })
         .catch(error => {
@@ -53,7 +73,9 @@ methods: {
     this.products.sort((a, b) => b.price - a.price);
   },
   handleSort(event) {
+    
     const value = event.target.value;
+   
     if (value === 'asc') {
       this.getAscending();
     } else if (value === 'desc') {
@@ -71,7 +93,7 @@ methods: {
 
 <div class="flex">
     <!-- Фиксированное боковое меню -->
-    <Aside @apply-filters="applyFilters"/>
+    <Sidebar />
   <div class="bg-white w-3/5 m-auto rounded-xl shadow-xl shadow-grey-200 mt-20">
   
 
@@ -81,7 +103,7 @@ methods: {
         <div class="flex items-center gap-4">
           <select
             class="py-2 px-3 border border-gray-200 focus:border-gray-400 rounded-md focus:outline-none"
-            @change="handleSort($event)"
+            @change.prevent="handleSort($event)"
           >
             <option value="name">По названию</option>
             <option value="asc">По цене (дешевые)</option>
@@ -111,8 +133,42 @@ methods: {
         /></div>
       </div>
     </div>
+
+    <div class="flex justify-center items-center mt-6">
+  <button 
+    class="py-2 px-4 mx-1 border rounded-md bg-gray-100 hover:bg-gray-200"
+    :disabled="pagination.current_page === 1"
+    @click="prevPage"
+  >
+    Назад
+  </button>
+
+  <button
+    v-for="page in pagination.last_page"
+    :key="page"
+    class="py-2 px-4 mx-1 border rounded-md"
+    :class="{'bg-blue-500 text-white': page === pagination.current_page}"
+    @click="goToPage(page)"
+  >
+    {{ page }}
+  </button>
+
+  <button 
+    class="py-2 px-4 mx-1 border rounded-md bg-gray-100 hover:bg-gray-200"
+    :disabled="pagination.current_page === pagination.last_page"
+    @click="nextPage"
+  >
+    Вперед
+  </button>
+</div>
+
   </div>
+
   </div>
+
+  
+
+
 </template>
 
 <style scoped>
@@ -129,6 +185,21 @@ aside {
   box-sizing: border-box;
   z-index: 10; /* Убедитесь, что оно не перекрывается */
 }
+
+button:disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+button.bg-blue-500 {
+  background-color: #3b82f6;
+  color: #fff;
+}
+
+button.bg-blue-500:hover {
+  background-color: #2563eb;
+}
+
 
 
 </style>
